9c61c5709e33ee7e4fa75f8246d1471ff086fbd6
Travis Brown <travisbrown@stripe.com>
2017-06-08 16:33:47 -0500

    Support merge keys

--- src/main/scala/io/circe/yaml/parser/package.scala
+++ src/main/scala/io/circe/yaml/parser/package.scala
@@ -3,9 +3,12 @@ package io.circe.yaml
 import cats.syntax.either._
 import io.circe._
 import java.io.{Reader, StringReader}
+import java.util.Date
 import org.yaml.snakeyaml.Yaml
+import org.yaml.snakeyaml.constructor.SafeConstructor
 import org.yaml.snakeyaml.nodes._
 import scala.collection.JavaConverters._
+import scala.util.Try
 
 package object parser {
 
@@ -17,12 +20,13 @@ package object parser {
     */
   def parse(yaml: Reader): Either[ParsingFailure, Json] = for {
     parsed <- parseSingle(yaml)
-    json   <- yamlToJson(parsed)
+    json   <- yamlToJson(new Flattener)(parsed)
   } yield json
 
   def parse(yaml: String): Either[ParsingFailure, Json] = parse(new StringReader(yaml))
 
-  def parseDocuments(yaml: Reader): Stream[Either[ParsingFailure, Json]] = parseStream(yaml).map(yamlToJson)
+  def parseDocuments(yaml: Reader): Stream[Either[ParsingFailure, Json]] =
+    parseStream(yaml).map(yamlToJson(new Flattener))
   def parseDocuments(yaml: String): Stream[Either[ParsingFailure, Json]] = parseDocuments(new StringReader(yaml))
 
   private[this] def parseSingle(reader: Reader) =
@@ -38,7 +42,20 @@ package object parser {
       None
   }
 
-  private[this] def yamlToJson(node: Node): Either[ParsingFailure, Json] = {
+  private[this] class Flattener extends SafeConstructor {
+    def flatten(node: MappingNode): MappingNode = {
+      flattenMapping(node)
+      node
+    }
+
+    def toDate(node: ScalarNode): Try[Date] = Try(constructObject(node).asInstanceOf[Date])
+  }
+
+  private[this] val timestampConstructor: SafeConstructor.ConstructYamlTimestamp =
+    new SafeConstructor.ConstructYamlTimestamp()
+
+
+  private[this] def yamlToJson(flattener: Flattener)(node: Node): Either[ParsingFailure, Json] = {
 
     def convertScalarNode(node: ScalarNode) = Either.catchNonFatal(node.getTag match {
       case Tag.INT | Tag.FLOAT => JsonNumber.fromString(node.getValue).map(Json.fromJsonNumber).getOrElse {
@@ -46,6 +63,8 @@ package object parser {
       }
       case Tag.BOOL => Json.fromBoolean(node.getValue.toBoolean)
       case Tag.NULL => Json.Null
+      case Tag.TIMESTAMP =>
+        flattener.toDate(node).map(date => Json.fromLong(date.getTime)).getOrElse(Json.fromString(node.getValue))
       case CustomTag(other) =>
         Json.fromJsonObject(JsonObject.singleton(other.stripPrefix("!"), Json.fromString(node.getValue)))
       case other => Json.fromString(node.getValue)
@@ -61,18 +80,20 @@ package object parser {
 
     node match {
       case mapping: MappingNode =>
-        mapping.getValue.asScala.foldLeft(Either.right[ParsingFailure, JsonObject](JsonObject.empty)) {
+        flattener.flatten(mapping).getValue.asScala.foldLeft(
+          Either.right[ParsingFailure, JsonObject](JsonObject.empty)
+        ) {
           (objEither, tup) => for {
             obj <- objEither
             key <- convertKeyNode(tup.getKeyNode)
-            value <- yamlToJson(tup.getValueNode)
+            value <- yamlToJson(flattener)(tup.getValueNode)
           } yield obj.add(key, value)
         }.map(Json.fromJsonObject)
       case sequence: SequenceNode =>
         sequence.getValue.asScala.foldLeft(Either.right[ParsingFailure, List[Json]](List.empty[Json])) {
           (arrEither, node) => for {
             arr <- arrEither
-            value <- yamlToJson(node)
+            value <- yamlToJson(flattener)(node)
           } yield value :: arr
         }.map(arr => Json.fromValues(arr.reverse))
       case scalar: ScalarNode => convertScalarNode(scalar)
