7cb8526745c958aee6ab3239dac46bb030edca62
Travis Brown <travisrobertbrown@gmail.com>
2019-11-12 15:34:32 +0100

    Merge pull request #43 from nornagon/parse-ints
    Parse integers using SnakeYAML's parser

--- src/test/scala/io/circe/yaml/ParserTests.scala
+++ src/test/scala/io/circe/yaml/ParserTests.scala
@@ -1,7 +1,9 @@
 package io.circe.yaml
 
 import io.circe.Json
-import org.scalatest.{EitherValues, FlatSpec, Matchers}
+import org.scalatest.{EitherValues, Matchers}
+import org.scalatest.flatspec.AnyFlatSpec
+import io.circe.syntax._
 
 class ParserTests extends FlatSpec with Matchers with EitherValues {
   // the laws should do a pretty good job of surfacing errors; these are mainly to ensure test coverage
@@ -29,6 +31,18 @@ class ParserTests extends FlatSpec with Matchers with EitherValues {
     assert(parser.parse(
       """foo: yes"""
     ).isRight)
+  }
+
+  it should "parse hexadecimal" in {
+    assert(parser.parse(
+      """[0xFF, 0xff, 0xab_cd]"""
+    ).contains(Seq(0xFF, 0xff, 0xabcd).asJson))
+  }
+
+  it should "parse decimal with underscore breaks" in {
+    assert(parser.parse(
+      """foo: 1_000_000"""
+    ).contains(Map("foo" -> 1000000).asJson))
   }
 
   it should "parse empty string as false" in {
