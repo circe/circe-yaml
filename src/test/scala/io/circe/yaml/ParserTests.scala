016b2134804322ce0884a9c3113eafd74235f5ca
Luca Violanti <luca.violanti@kaluza.com>
2022-11-14 15:54:21 +0000

    Add tests to `Parser.parseDocuments`
    Some tests highlight a divergence from `Parser.parse` which might be
    undesirable - see comments

--- src/test/scala/io/circe/yaml/ParserTests.scala
+++ src/test/scala/io/circe/yaml/ParserTests.scala
@@ -1,15 +1,17 @@
 package io.circe.yaml
 
+import cats.syntax.all._
 import io.circe.Json
+import io.circe.syntax._
+import java.io.StringReader
 import org.scalatest.EitherValues
 import org.scalatest.flatspec.AnyFlatSpec
-import io.circe.syntax._
 import org.scalatest.matchers.should.Matchers
 
 class ParserTests extends AnyFlatSpec with Matchers with EitherValues {
   // the laws should do a pretty good job of surfacing errors; these are mainly to ensure test coverage
 
-  "Parser" should "fail on invalid tagged numbers" in {
+  "Parser.parse" should "fail on invalid tagged numbers" in {
     assert(parser.parse("!!int 12foo").isLeft)
   }
 
@@ -121,5 +123,133 @@ class ParserTests extends AnyFlatSpec with Matchers with EitherValues {
         )
         .isLeft
     )
+  }
+
+  "Parser.parseDocuments" should "fail on invalid tagged numbers" in {
+    val result = parser.parseDocuments(new StringReader("!!int 12foo")).toList
+    assert(result.size == 1)
+    assert(result.head.isLeft)
+  }
+
+  it should "fail to parse complex keys" in {
+    val result = parser
+      .parseDocuments(new StringReader("""
+        |? - foo
+        |  - bar
+        |: 1""".stripMargin))
+      .toList
+    assert(result.size == 1)
+    assert(result.head.isLeft)
+  }
+
+  it should "fail to parse invalid YAML" in {
+    /*
+    See comments below that document the "real" expected behaviour
+     */
+    val result = Either.catchNonFatal(parser.parseDocuments(new StringReader("""foo: - bar""")).toList)
+    assert(result.isLeft)
+    assert(result.swap.value.isInstanceOf[org.yaml.snakeyaml.scanner.ScannerException])
+    // val result = parser.parseDocuments(new StringReader("""foo: - bar""")).toList
+    // assert(result.size == 1)
+    // assert(result.head.isLeft)
+    // assert(result.head.isInstanceOf[Either[io.circe.ParsingFailure, Json]])
+  }
+
+  it should "parse yes as true" in {
+    val result = parser.parseDocuments(new StringReader("""foo: yes""")).toList
+    assert(result.size == 1)
+    assert(result.head.isRight)
+  }
+
+  it should "parse hexadecimal" in {
+    val result = parser.parseDocuments(new StringReader("""[0xFF, 0xff, 0xab_cd]""")).toList
+    assert(result.size == 1)
+    assert(result.head.contains(Seq(0xff, 0xff, 0xabcd).asJson))
+  }
+
+  it should "parse decimal with underscore breaks" in {
+    val result = parser.parseDocuments(new StringReader("""foo: 1_000_000""")).toList
+    assert(result.size == 1)
+    assert(result.head.contains(Map("foo" -> 1000000).asJson))
+  }
+
+  it should "parse empty string as false" in {
+    /*
+    See comments below that document the "real" expected behaviour
+     */
+    val result = parser.parseDocuments(new StringReader("")).toList
+    assert(result.isEmpty)
+    // assert(result.size == 1)
+    // assert(result.head.right.value == Json.False)
+  }
+
+  it should "parse blank string as false" in {
+    /*
+    See comments below that document the "real" expected behaviour
+     */
+    val result = parser.parseDocuments(new StringReader("   ")).toList
+    assert(result.isEmpty)
+    // assert(result.size == 1)
+    // assert(result.head.right.value == Json.False)
+  }
+
+  it should "parse aliases" in {
+    val result = parser
+      .parseDocuments(
+        new StringReader(
+          """
+            | aliases:
+            |   - &alias1
+            |     foo:
+            |       bar
+            | baz:
+            |  - *alias1
+            |  - *alias1
+            |""".stripMargin
+        )
+      )
+      .toList
+    assert(result.size == 1)
+    assert(result.head.isRight)
+  }
+
+  it should "fail to parse too many aliases" in {
+    /*
+    See comments below that document the "real" expected behaviour
+     */
+    val result = Either.catchNonFatal(
+      Parser(maxAliasesForCollections = 1)
+        .parseDocuments(
+          new StringReader(
+            """
+                | aliases:
+                |   - &alias1
+                |     foo:
+                |       bar
+                | baz:
+                |  - *alias1
+                |  - *alias1
+                |""".stripMargin
+          )
+        )
+        .toList
+    )
+    assert(result.isLeft)
+    assert(result.swap.value.isInstanceOf[org.yaml.snakeyaml.error.YAMLException])
+    // val result =
+    //   Parser(maxAliasesForCollections = 1)
+    //     .parseDocuments(new StringReader(
+    //       """
+    //         | aliases:
+    //         |   - &alias1
+    //         |     foo:
+    //         |       bar
+    //         | baz:
+    //         |  - *alias1
+    //         |  - *alias1
+    //         |""".stripMargin
+    //     )).toList
+    // assert(result.isEmpty)
+    // assert(result.head.isLeft)
   }
 }
