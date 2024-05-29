d4580adf96a574a66b655ae9435fbb44d2a1c282
Ondra Pelech <ondra.pelech@gmail.com>
2024-05-29 12:56:18 +0200

    Simplify, rely on scala-yaml more

--- circe-yaml-scalayaml/shared/src/main/scala/io/circe/yaml/scalayaml/Parser.scala
+++ circe-yaml-scalayaml/shared/src/main/scala/io/circe/yaml/scalayaml/Parser.scala
@@ -21,13 +21,11 @@ import cats.syntax.all._
 import io.circe
 import io.circe.Decoder
 import io.circe.Json
-import io.circe.JsonNumber
 import io.circe.ParsingFailure
 import org.virtuslab.yaml._
 
 import java.io.Reader
 import scala.collection.mutable
-import scala.util.Try
 
 object Parser extends io.circe.yaml.common.Parser {
 
@@ -73,36 +71,29 @@ object Parser extends io.circe.yaml.common.Parser {
     }
   }
 
+  private def scalarNodeToJson(node: Node.ScalarNode): Either[ParsingFailure, Json] = {
+    val parsed = YamlDecoder.forAny.construct(node).left.map(errorToFailure)
+    parsed.flatMap {
+      case null | None   => Json.Null.asRight
+      case value: String => Json.fromString(value).asRight
+      case value: Int    => Json.fromInt(value).asRight
+      case double: Double =>
+        Json.fromDouble(double).toRight(ParsingFailure(s"${node.value} cannot be represented as a JSON number.", null))
+      case value: Boolean => Json.fromBoolean(value).asRight
+      case value: Long    => Json.fromLong(value).asRight
+      case float: Float =>
+        Json.fromFloat(float).toRight(ParsingFailure(s"${node.value} cannot be represented as a JSON number.", null))
+      case value: BigDecimal => Json.fromBigDecimal(value).asRight
+      case value: BigInt     => Json.fromBigInt(value).asRight
+      case value: Byte       => Json.fromInt(value.toInt).asRight
+      case value: Short      => Json.fromInt(value.toInt).asRight
+      case value => ParsingFailure(s"Can't map ${value.getClass.getSimpleName} (${node.value}) to JSON.", null).asLeft
+    }
+  }
+
   private def nodeToJson(node: Node): Either[ParsingFailure, Json] = node match {
-    case Node.ScalarNode(value, tag) =>
-      tag.value match {
-        case "tag:yaml.org,2002:str" =>
-          Right(Json.fromString(value))
-        case "tag:yaml.org,2002:null" =>
-          value match {
-            case "null" => Right(Json.Null)
-            case _      => Left(ParsingFailure(s"Expected 'null', but got $value.", null))
-          }
-        case "tag:yaml.org,2002:bool" =>
-          value.toBooleanOption.map(Json.fromBoolean).toRight(ParsingFailure(s"Can't parse '$value' as bool.", null))
-        case "tag:yaml.org,2002:int" =>
-          Try(java.lang.Long.decode(value.replaceAll("_", ""))).toEither
-            .map(l => Json.fromLong(l))
-            .left
-            .map(ParsingFailure(s"Can't parse '$value' as int.", _))
-        case "tag:yaml.org,2002:float" =>
-          Try(java.lang.Double.parseDouble(value.replaceAll("_", ""))).toEither
-            .flatMap(d => Json.fromDouble(d).toRight(new Exception("Argument cannot be represented as a JSON number.")))
-            .left
-            .map(ParsingFailure(s"Can't parse '$value' as float.", _))
-        case _ =>
-          val nil = Option.when(value == "null")(Json.Null)
-          val num = JsonNumber.fromString(value).map(Json.fromJsonNumber)
-          val bol = value.toBooleanOption.map(Json.fromBoolean)
-          val str = Json.fromString(value)
-          val result = nil.orElse(num).orElse(bol).getOrElse(str)
-          Right(result)
-      }
+    case node: Node.ScalarNode =>
+      scalarNodeToJson(node)
     case Node.SequenceNode(nodes, _) =>
       val values = nodes.traverse(nodeToJson)
       values.map(Json.fromValues)
--- circe-yaml-scalayaml/shared/src/main/scala/io/circe/yaml/scalayaml/Printer.scala
+++ circe-yaml-scalayaml/shared/src/main/scala/io/circe/yaml/scalayaml/Printer.scala
@@ -28,12 +28,11 @@ object Printer extends io.circe.yaml.common.Printer {
   }
 
   private def jsonToNode(json: Json): Node = json match {
-    case Json.JNull          => Node.ScalarNode("null")
-    case json: Json.JBoolean => Node.ScalarNode(json.toString)
-    case json: Json.JNumber  => Node.ScalarNode(json.toString)
-    case json: Json.JString  => Node.ScalarNode(json.toString)
-    case Json.JArray(value)  => Node.SequenceNode(value.map(jsonToNode): _*)
+    case Json.JArray(value) =>
+      Node.SequenceNode(value.map(jsonToNode): _*)
     case Json.JObject(value) =>
       Node.MappingNode(value.toMap.map { case (key, value) => (Node.ScalarNode(key): Node) -> jsonToNode(value) })
+    case json =>
+      Node.ScalarNode(json.toString)
   }
 }
